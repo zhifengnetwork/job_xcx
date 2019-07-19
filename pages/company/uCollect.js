5328b41f3e69128261080310ddfe7dd512499bba
unknown <1017074715@qq.com>
2019-07-19 14:14:56 +0800

    111

--- pages/company/uCollect.js
+++ pages/company/uCollect.js
@@ -12,7 +12,16 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+      var that = this;
+      wx.request({
+        url: 'collection/collection_list',
+        data:{
 
+        },
+        success: function (res) {
+          console.log(res)
+        }
+      })
   },
 
   /**
