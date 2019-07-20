7cc445029c454d6d4a25ffb49d1c5fb85f9ecef4
unknown <1017074715@qq.com>
2019-07-20 12:05:48 +0800

    xx

--- pages/company/postionDetail.js
+++ pages/company/postionDetail.js
@@ -1,15 +1,57 @@
 // pages/company/postionDetail.js
+import ServerData from '../../utils/serverData.js';
+const app=getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    Id:'2',
   },
   callWithHim:function(){
     wx.makePhoneCall({
       phoneNumber: '18365478951' // 仅为示例，并非真实的电话号码
+    })
+  },
+  // 收藏/取消收藏
+  Oncollection:function() {
+    var that = this,
+    toId = that.data.toId;
+    wx.request({
+      url:app.globalData.baseUrl+'/collection/collection',
+      data:{
+        token:wx.getStorageSync('token'),
+        type:wx.getStorageSync('savePostion'),
+        to_id:toId
+      },
+      method:'post',
+      success:(res)=>{
+        console.log(res)
+        if (res.data.status == 1) {
+            
+        }
+      }
+    })
+  },
+  // 公司职位详情数据
+  TheDetails:function() {
+    var that = this,
+    Id = that.data.Id;
+    wx.request({
+      url:app.globalData.baseUrl+'/company/recruit_detail',
+      data:{
+        token:wx.getStorageSync('token'),
+        // type:wx.getStorageSync('savePostion'),
+        id:Id
+      },
+      method:'post',
+      success:(res)=>{
+        console.log(res)
+        if (res.data.status == 1) {
+            
+        }
+      }
     })
   },
   /**
@@ -23,7 +65,7 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    this.TheDetails();
   },
 
   /**
